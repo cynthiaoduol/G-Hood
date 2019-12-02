d11c413c01b0eadee6798d50fa51f590ff3c8008
cynthiaoduol <cynthiaobu940@gmail.com>
2019-12-02 23:03:16 +0300

    import receiver

--- ghood/models.py
+++ ghood/models.py
@@ -15,14 +15,14 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
-    # @receiver(post_save, sender=User)
-    # def create_user_profile(sender, instance, created, **kwargs):
-    #     if created:
-    #         Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
-    # @receiver(post_save, sender=User)
-    # def save_user_profile(sender, instance, **kwargs):
-    #     instance.profile.save()
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 
 class Neighbourhood(models.Model):
